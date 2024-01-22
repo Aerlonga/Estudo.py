84f97a250acf40776191f859ecb03cfabdaa8489
Aerlonga <aerlon.as@gmail.com>
2024-01-22 14:21:46 -0300

    att

--- aula/aula1.py
+++ aula/aula1.py
@@ -1,4 +1,9 @@
-print("odhsaoidq")
-print("odhsaoidq")
-print("odhsaoidq")
-print("odhsaoidq")
+a = "A"
+b = "B"
+c = 1.1
+string = "a = {} b = {} c = {:.2f}"
+
+
+format = string.format(a, b, c)
+
+print(format)
