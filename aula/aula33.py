02eb4485d908117c5569c1a9af4e2fb94277e93e
Aerlonga <aerlon.as@gmail.com>
2024-01-22 15:14:31 -0300

    att

--- aula/aula33.py
+++ aula/aula33.py
@@ -5,4 +5,4 @@ numero_2 = input("Digite outro numero: ")
 int_numero1 = int(numero_1)
 int_numero2 = int(numero_2)
 
-print(f"A soma dos números é: {int_numero1 + int_numero2}")
+print(f"A soma dos números é: {int_numero1 + int_numero2}") #F é usado para formatar uma string igual ´${}´ em javascript
