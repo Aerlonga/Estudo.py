86d0ee1787f2d62cbaea3ffa4efd1b99ff985f1e
Aerlonga <aerlon.as@gmail.com>
2024-01-22 15:01:08 -0300

    att

---
+++ aula/aula33.py
@@ -0,0 +1,8 @@
+numero_1 = input("Digite um numero: ")
+numero_2 = input("Digite outro numero: ")
+
+
+int_numero1 = int(numero_1)
+int_numero2 = int(numero_2)
+
+print(f"A soma dos números é: {int_numero1 + int_numero2}")
